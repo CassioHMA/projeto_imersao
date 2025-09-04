5ce944dd97a9abb5b72e7d530ae97a6101bdb11e
cassioHMA <cassiohemachado@gmail.com>
2025-09-04 20:59:15 -0300

    Criação das Tabela Produto e Aluguel

--- Projeto_imersaos/admin.py
+++ Projeto_imersaos/admin.py
@@ -1,4 +1,4 @@
 from django.contrib import admin
-from .models import Cadastro
+from .models import Cadastro, Produto, Aluguel
 
 admin.site.register(Cadastro)
--- Projeto_imersaos/models.py
+++ Projeto_imersaos/models.py
@@ -9,4 +9,22 @@ class Cadastro(models.Model):
 
     def __str__(self):  
         return self.nome + " - " + str(self.idade)  + " - " + self.locacao + " - " + self.contato
-# Create your models here.
+
+class Produto(models.Model):
+    nome = models.CharField(max_length=200)
+    descricao = models.TextField()
+    preco = models.DecimalField(max_digits=10, decimal_places=2)
+    estoque = models.IntegerField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.nome + " - " + str(self.preco) + " - " + str(self.estoque)
+    
+class Aluguel(models.Model):
+    produto = models.ForeignKey(Produto, on_delete=models.CASCADE)
+    cliente = models.ForeignKey(Cadastro, on_delete=models.CASCADE)
+    data_aluguel = models.DateTimeField(auto_now_add=True)
+    data_devolucao = models.DateTimeField(null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.cliente.nome} alugou {self.produto.nome} em {self.data_aluguel}"
