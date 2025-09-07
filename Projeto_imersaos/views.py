d123f00d814f091010bfa3f97aa94c276800b06c
cassioHMA <cassiohemachado@gmail.com>
2025-09-07 20:06:12 -0300

    inclusao do inicio do html

--- Projeto_imersaos/views.py
+++ Projeto_imersaos/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
+def index(request):
+    """"Página Principal do Pereirao Projeto_imersao"""
+    return render(request, 'projeto_imersao/index.html')
+
 # Create your views here.
